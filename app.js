53ebb1406b0d6ba9c296b2963e4dbf7b4612a6cc
winphong <winphong@comp.nus.edu.sg>
2019-09-01 19:44:40 +0800

    changed CSP

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-require("./startup/db")();
 require("./startup/cors")(app);
+require("./startup/db")();
 require("./startup/routes")(app);
 
 app.use(express.static(path.join(__dirname, "images")));
