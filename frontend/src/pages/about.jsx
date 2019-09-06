31baf25e37f40d4d9cacb54810035f799e73fd42
winphong <winphong@comp.nus.edu.sg>
2019-09-06 18:34:21 +0800

    change vh/vw to %

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -27,6 +27,10 @@ const styles = theme => ({
     border: 1,
     borderColor: "black",
     backgroundColor: "transparent"
+  },
+  staticImage: {
+    width: "75%",
+    height: "75%"
   }
 });
 
@@ -79,81 +83,32 @@ class About extends Component {
                 <Grid item xs={12} sm={9}>
                   <Grid container spacing={0}>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{
-                          width: "8.5vw",
-                          height: "17vh",
-                          backgroundColor: "pink"
-                        }}
-                        src="/RH.png"
-                      />
+                      <img className={classes.staticImage} src="/RH.png" />
                       <p>Raffles Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{
-                          width: "8.5vw",
-                          height: "17vh",
-                          backgroundColor: "pink"
-                        }}
-                        src="/EH.png"
-                      />
+                      <img className={classes.staticImage} src="/EH.png" />
                       <p>Eusoff Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{
-                          width: "8.5vw",
-                          height: "17vh",
-                          backgroundColor: "pink"
-                        }}
-                        src="/TH.png"
-                      />
+                      <img className={classes.staticImage} src="/TH.png" />
                       <p> Temasek Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{
-                          width: "8.5vw",
-                          height: "17vh",
-                          backgroundColor: "pink"
-                        }}
-                        src="/SH.png"
-                      />
+                      <img className={classes.staticImage} src="/SH.png" />
                       <p> Sheares Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{
-                          width: "8.5vw",
-                          height: "17vh",
-                          backgroundColor: "pink"
-                        }}
-                        src="/KE.png"
-                      />
+                      <img className={classes.staticImage} src="/KE.png" />
                       <p> King Edward VII Hall</p>
                       <p />
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{
-                          width: "8.5vw",
-                          height: "17vh",
-                          backgroundColor: "pink"
-                        }}
-                        src="/KR.png"
-                      />
+                      <img className={classes.staticImage} src="/KR.png" />
                       <p> Kent Ridge Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{
-                          width: "8.5vw",
-                          height: "17vh",
-                          backgroundColor: "pink"
-                        }}
-                        src="/PGP.png"
-                      />
+                      <img className={classes.staticImage} src="/PGP.png" />
                       <p>Prince George's Park Residence</p>
                     </Grid>
                   </Grid>
@@ -169,35 +124,32 @@ class About extends Component {
                   <Grid container spacing={0}>
                     <Grid item xs={12} sm={3} />
                     <Grid item xs={12} sm={3}>
-                      <img src="https://static.wixstatic.com/media/75612e_8c531011a2024f2f899dfbd889ad82db~mv2.png/v1/fill/w_105,h_113,al_c,q_80,usm_0.66_1.00_0.01/RH%20logo.webp" />
+                      <img className={classes.staticImage} src="/RH.png" />
                       <p>Raffles Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img src="https://static.wixstatic.com/media/75612e_a66243c354444b14af625f83064b7041~mv2.png/v1/fill/w_90,h_115,al_c,q_80,usm_0.66_1.00_0.01/EH%20logo.webp" />
+                      <img className={classes.staticImage} src="/EH.png" />
                       <p>Eusoff Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img
-                        style={{ width: "25%" }}
-                        src="https://static.wixstatic.com/media/75612e_7a5b17ce7add491cb81c2e43d8762f58~mv2.jpg/v1/fill/w_202,h_278,al_c,q_80,usm_0.66_1.00_0.01/TH%20logo.webp"
-                      />
+                      <img className={classes.staticImage} src="/TH.png" />
                       <p> Temasek Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img src="https://static.wixstatic.com/media/75612e_bb53dd7f9e0b4ca39bc6fd9ff47de689~mv2.jpg/v1/fill/w_127,h_124,al_c,q_80,usm_0.66_1.00_0.01/SH%20logo.webp" />
+                      <img className={classes.staticImage} src="/SH.png" />
                       <p> Sheares Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img src="https://static.wixstatic.com/media/75612e_ae210bf29d314e55a4b45690125682f3~mv2.png/v1/fill/w_99,h_109,al_c,q_80,usm_0.66_1.00_0.01/hall-crest-vector.webp" />
+                      <img className={classes.staticImage} src="/KE.png" />
                       <p> King Edward VII Hall</p>
                       <p />
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img src="https://static.wixstatic.com/media/75612e_008e77074a08483e87c5bdc12a28d648~mv2.jpg/v1/fill/w_121,h_113,al_c,q_80,usm_0.66_1.00_0.01/KR%20logo.webp" />
+                      <img className={classes.staticImage} src="/KR.png" />
                       <p> Kent Ridge Hall</p>
                     </Grid>
                     <Grid item xs={12} sm={3}>
-                      <img src="https://static.wixstatic.com/media/75612e_0e7db723601d4b8ea7de9b44a12af805~mv2.png/v1/fill/w_122,h_122,al_c,q_80,usm_0.66_1.00_0.01/pgp.webp" />
+                      <img className={classes.staticImage} src="/PGP.png" />
                       <p>Prince George's Park Residence</p>
                     </Grid>
                   </Grid>
