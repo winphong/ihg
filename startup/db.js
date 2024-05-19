35850b0f0c8adcb8b48f18f7354f90ce31c8ab4b
winphong <winphong1011@gmail.com>
2024-05-19 23:59:51 +0800

    build: fix build scripts

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-module.exports = function() {
+module.exports = function () {
   const db = config.get("db");
   mongoose
     .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     })
-    .then(() => console.log(`Connected to ${db}...`));
+    .then(() => console.log(`Connected to ${db}...`))
+    .catch((err) => console.error("Unable to connect to database"));
 };
