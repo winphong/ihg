a8a1a115c7a684b40cf1eb9550945775a6c19b0c
winphong <winphong1011@gmail.com>
2024-05-20 00:44:05 +0800

    fix: change path to server static file

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ require("./startup/cors")(app);
 require("./startup/db")();
 require("./startup/routes")(app);
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "frontend", "public")));
 
 const port = process.env.PORT || config.get("port");
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "public", "index.html"));
 });
 
 const server = app.listen(port, () => {
